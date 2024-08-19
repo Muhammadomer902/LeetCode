b1173ab2bad0c98c1ea3e741b52a05aecc52f59a
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-19 21:10:40 +0500

    [650] [Time Beats: 100.00%] [Memory Beats: 76.32%] - LeetPush

---
+++ DailyProblems/Day_1/650-2-Keys-Keyboard.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if(n>=1 && n<=1000)
+        {
+            if(n==1)
+            {
+                return 0;
+            }
+            int result = 0;
+            for (int i = 2; i <= n; i++) {
+                while (n % i == 0) 
+                { 
+                    result += i; 
+                    n /= i;
+                }
+            }
+            return result;
+        }
+        return 0;
+    }
+};
