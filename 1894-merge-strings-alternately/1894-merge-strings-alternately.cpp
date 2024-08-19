1734ce505cbc627b5e3b8e6f47c420650aa966df
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-19 19:15:40 +0500

    Time: 7 ms (21.82%), Space: 9.7 MB (5.84%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0,j=0;
+        string merged ="";
+        while(word1[i]!='\0' && word2[j]!='\0')
+        {
+            merged = merged+word1[i]+word2[j];
+            i++;j++;
+        }
+        if(word1.length()>word2.length())
+        {
+            while(word1[i]!='\0')
+            {
+                merged=merged+word1[i];
+                i++;
+            }
+        }
+        else if(word1.length()<word2.length())
+        {
+            while(word2[j]!='\0')
+            {
+                merged=merged+word2[j];
+                j++;
+            }
+        }
+        return merged;
+    }
+};
