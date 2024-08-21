dcea134069d1b710892360f2443c26c798d4996f
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-21 20:15:21 +0500

    Time: 12 ms (94.9%), Space: 11.6 MB (33.04%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
-        max = 0
         size = len(candies)
         arr = [False]*size
-        for i in candies:
-            if i>max:
-                max = i
         for i in range(size):
-            if candies[i]+extraCandies>=max:
+            if candies[i]+extraCandies>=max(candies):
                 arr[i]=True
         return arr
