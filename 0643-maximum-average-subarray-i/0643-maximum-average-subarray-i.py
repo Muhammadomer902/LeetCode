25e68a4b4762a069c3d92a9d48f89a2c1bb3b9a4
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-27 21:19:54 +0500

    Delete 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findMaxAverage(self, nums, k):
-        window_sum = float(sum(nums[:k]))
-        max_avg = float(window_sum / k)
-        for i in range(k, len(nums)):
-            window_sum += nums[i] - nums[i - k]
-            max_avg = max(max_avg, window_sum / k)
-        
-        return max_avg
