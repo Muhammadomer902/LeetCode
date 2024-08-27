d5cd1c806a99a264dd12324a2d25c82f85f78ca3
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-27 21:38:34 +0500

    Create cont.py

---
+++ 1732-find-the-highest-altitude/cont.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        max = 0;sum = 0
+        for i in gain:
+            sum+=i
+            if sum>max:
+                max = sum
+        return max
