1df16cb34e619cbbdca42f73eb80e0599af710a1
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-08-31 11:46:05 +0500

    Time: 478 ms (99.64%), Space: 25.2 MB (83.83%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -2,14 +2,6 @@ import heapq
 
 class Solution(object):
     def maxProbability(self, n, edges, succProb, start_node, end_node):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :type succProb: List[float]
-        :type start_node: int
-        :type end_node: int
-        :rtype: float
-        """
         graph = [[] for _ in range(n)]
         for i, (u, v) in enumerate(edges):
             graph[u].append((v, succProb[i]))
