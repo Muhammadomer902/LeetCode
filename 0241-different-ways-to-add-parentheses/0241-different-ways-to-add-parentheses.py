89713f00465eca6d8225f8b68d3501111009ca04
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-09-19 09:41:22 +0500

    Time: 13 ms (87.5%), Space: 12 MB (12.07%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def diffWaysToCompute(self, expression):
+        memo = {}
+
+        def compute(expression):
+            if expression in memo:
+                return memo[expression]
+        
+            result = []
+            for i, char in enumerate(expression):
+                if char in "+-*":
+                    left = compute(expression[:i])
+                    right = compute(expression[i+1:])
+                
+                    for l in left:
+                        for r in right:
+                            if char == '+':
+                                result.append(l + r)
+                            elif char == '-':
+                                result.append(l - r)
+                            elif char == '*':
+                                result.append(l * r)
+        
+            if not result:
+                result.append(int(expression))
+        
+            memo[expression] = result
+            return result
+
+        return compute(expression)
