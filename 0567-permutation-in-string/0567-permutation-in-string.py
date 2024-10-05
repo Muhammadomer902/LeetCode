0e5e04369955760774d425473626fd439d7aa2cf
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-10-05 10:55:04 +0500

    Time: 60 ms (40.61%), Space: 12.1 MB (55.03%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        from collections import Counter
+        if len(s1) > len(s2):
+            return False
+        s1_count = Counter(s1)
+        window_count = Counter(s2[:len(s1)])
+        if s1_count == window_count:
+            return True
+        for i in range(len(s1), len(s2)):
+            start_char = s2[i - len(s1)]
+            window_count[start_char] -= 1
+            if window_count[start_char] == 0:
+                del window_count[start_char]
+            window_count[s2[i]] += 1
+            if window_count == s1_count:
+                return True
+
+        return False
