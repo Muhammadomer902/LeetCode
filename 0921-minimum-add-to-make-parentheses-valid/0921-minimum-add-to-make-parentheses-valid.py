ab58a41d21076bf6a2d26f9bd28e8ad474293ea5
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-10-09 18:50:17 +0500

    Time: 9 ms (91.71%), Space: 11.5 MB (76.5%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        open_needed = 0
+        close_needed = 0
+        
+        for char in s:
+            if char == '(':
+                close_needed += 1  
+            elif char == ')':
+                if close_needed > 0:
+                    close_needed -= 1  
+                else:
+                    open_needed += 1  
+
+        return open_needed + close_needed
