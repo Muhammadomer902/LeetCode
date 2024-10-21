6420909d34f048f11bf7cfabd3c95134ec3fcffb
Muhammad omer <sayyidmuhammadomer@gmail.com>
2024-10-21 18:35:31 +0500

    Time: 191 ms (92.86%), Space: 11.7 MB (64.29%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -4,22 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Backtracking function
         def backtrack(start, seen):
-            # If we've reached the end of the string, return 0
             if start == len(s):
                 return 0
-            
             max_count = 0
-            
-            # Try all possible substrings starting from index `start`
             for end in range(start + 1, len(s) + 1):
                 substring = s[start:end]
-                
-                # If the substring is not seen, proceed with it
                 if substring not in seen:
                     seen.add(substring)
-                    # Recursively backtrack for the remaining part of the string
                     count = 1 + backtrack(end, seen)
                     max_count = max(max_count, count)
                     seen.remove(substring)  
