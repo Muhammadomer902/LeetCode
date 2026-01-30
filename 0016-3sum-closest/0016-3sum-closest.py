6594a833bc1fe0493a028bf60716bc7684abcfc6
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-01-30 19:07:23 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n=100000000000
+        size = len(nums)
+        nums = sorted(nums)
+        for i in range(0,size-2):
+
+            if i > 0  and nums[i] == nums[i-1]:
+                continue
+            
+            l,r = i+1,size-1
+
+            while l<r:
+                sumt = nums[i] + nums[l] + nums[r]
+
+                if abs(target - sumt) <= abs(target - n):
+                    n =  sumt 
+
+                if sumt > 0:
+                    r-=1
+                elif sumt<0:
+                    l+=1
+                else:
+                    l+=1
+                    
+        return n
+
