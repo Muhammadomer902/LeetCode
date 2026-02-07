1eb5d4459b000f5321da68a2574fa5719ec075a8
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-02-07 21:13:14 +0500

    Time: 3 ms (58.05%), Space: 12.7 MB (23.42%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -6,13 +6,12 @@ class Solution(object):
         """
         rpath=[]
         path = path.split('/')
-        print(path)
         for i in path:
             if i == ".." and rpath:
                 rpath.pop()
             elif i!='' and i!=".." and i!='.':
                 rpath.append(i)
-        print(rpath)
+
         rtpath =""
 
         for i in rpath:
