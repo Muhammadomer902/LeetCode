4697893a2e1d63ffb173486911c8ba4e4e806277
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-02-08 23:38:38 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        arr = [];count = 0;i = head
+        while i.next!=None:
+            if i not in arr:
+                arr.append(i)
+                count+=1
+            else:
+                return True
+            i = i.next
+        return False
