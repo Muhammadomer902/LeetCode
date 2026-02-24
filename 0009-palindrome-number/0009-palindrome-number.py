dd6b3a9373647db762e3b86b73d185de618cadf4
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-02-24 11:31:51 +0500

    Time: 4 ms (90.32%), Space: 12.5 MB (56.22%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        arr = []
-        if x<0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        while x>0:
-            digit=x%10
-            x=x/10
-            arr.insert(0,digit)
-        left = 0;right = len(arr)-1
-        while left<right:
-            if arr[left]!=arr[right]:
-                return False
-            left+=1
-            right-=1
-        return True
+        reversedhalf = 0
+        
+        while x > reversedhalf:
+            reversedhalf = reversedhalf * 10 + x % 10
+            x //= 10
+        
+        return x == reversedhalf or x == reversedhalf // 10
