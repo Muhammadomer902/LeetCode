63b7a0759b0535e2bff6ca29a1faabd23a025b31
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-02-26 17:09:56 +0500

    Time: 24 ms (16.76%), Space: 12.3 MB (85.22%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        rnum = 0;flag = False
+        if x<0:
+            flag = True
+            x = x*-1
+        while x:
+            digit = x%10
+            x = x/10
+            rnum = rnum*10 + digit
+        if rnum>2147483647 or rnum<-2147483648:
+            return 0
+        if flag:
+            return -1*rnum
+        else:
+            return rnum
