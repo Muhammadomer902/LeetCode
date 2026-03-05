7ac87c900c9af9ebcd2b2dd302c8d9d86dc3177a
Muhammad omer <sayyidmuhammadomer@gmail.com>
2026-03-05 22:26:08 +0500

    Time: 0 ms (100%), Space: 12.4 MB (53.6%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        count = 0
+        for i in range(0,len(nums)):
+            if nums[i]!=val:
+                nums[count]=nums[i]
+                count+=1
+        return count
